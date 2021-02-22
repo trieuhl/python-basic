fe53bf16c012c153761c0b0b1eba51562e3ce611
trieuhl <longtrieujp@gmail.com>
2021-02-22 10:13:44 +0900

    add output

--- introductory/lesson1_hello_world.py
+++ introductory/lesson1_hello_world.py
@@ -1,3 +1,8 @@
 if __name__ == '__main__':
     print("hello world")
     print("Toi yeu Viet Nam")
+
+    # --------- output ----------
+    # hello world
+    # Toi yeu Viet Nam
+    # --------- output ----------
