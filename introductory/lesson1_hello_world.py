f12035ea3d800e178f93ca9d9343194b1628387f
trieuhl <longtrieujp@gmail.com>
2021-02-22 10:20:39 +0900

    update lesson 1 output

--- introductory/lesson1_hello_world.py
+++ introductory/lesson1_hello_world.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
     print("hello world")
-    print("Toi yeu Viet Nam")
 
-    # --------- output ----------
-    # hello world
-    # Toi yeu Viet Nam
-    # --------- output ----------
+    """
+    # ------------- OUTPUT -------------
+    hello world
+    # ------------- OUTPUT -------------
+    """
