4039cd2c5ca26698d28d18e06569a1f0b5fa9f1b
trieuhl <longtrieujp@gmail.com>
2021-03-25 11:36:45 +0900

    update bai 10.3 buoc 1

--- introductory/exercises/lesson10/ex13_docchuso_buoc1.py
+++ introductory/exercises/lesson10/ex13_docchuso_buoc1.py
@@ -2,13 +2,14 @@ if __name__ == '__main__':
 
     n = 123
     print("n = ", n)
+    print("-----------------")
 
     # n = abc
     # buoc 1: tinh chu so hang tram (a)
     a = n // 100
     print("a = ", a)
     print("cach doc: ", a, "TRAM")
-    print()
+    print("-----------------")
 
     # buoc 2: lay ra bc
     bc = n % 100
@@ -18,22 +19,25 @@ if __name__ == '__main__':
     b = bc // 10
     print("b = ", b)
     print("cach doc: ", b, "CHUC")
+    print("-----------------")
 
     # buoc 4: lay ra c
     c = bc % 10
     print("c = ", c)
-    print("cach doc: ", c)
+    print("cach doc: ", c, "DON VI")
 
     """
     # ------------- OUTPUT -------------
     n =  123
+    -----------------
     a =  1
     cach doc:  1 TRAM
-    
+    -----------------
     bc =  23
     b =  2
     cach doc:  2 CHUC
+    -----------------
     c =  3
-    cach doc:  3
+    cach doc:  3 DON VI
     # ------------- OUTPUT -------------
     """
